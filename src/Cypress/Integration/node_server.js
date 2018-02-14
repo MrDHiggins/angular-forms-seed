172477c76744104b3700eaf85150918b15992f10
h5media <dyllanhiggins7@gmail.com>
2018-02-14 11:32:50 +0200

    Add files via upload

---
+++ src/Cypress/Integration/node_server.js
@@ -0,0 +1,40 @@
+// imports
+var http = require('http');
+var formidable = require('formidable');
+var util = require('util');
+
+
+// create server instance
+var server = http.createServer(function (req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  
+  if(req.method.toLowerCase() === 'post'){
+    processForm(req, res);
+    return;
+  }
+  res.end();
+});
+
+// processes submitted form
+function processForm(req, res) {
+  var form = new formidable.IncomingForm();
+  
+  form.parse(req, function (err, fields) {
+    res.writeHead(200, {
+      'content-type': 'text/plain'
+    });
+    res.end(util.inspect({
+      fields: fields
+    }));
+    console.log('posted fields: \n');
+    console.log(util.inspect({
+      fields: fields
+    }));
+  });
+}
+
+
+var port = 3100;
+server.listen(port);
+console.log('Server listening on port ' + port);
